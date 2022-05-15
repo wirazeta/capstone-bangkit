31745c966146a578c5baa7329c34f77098d3a2d5
wirazeta <dontknow1213@gmail.com>
2022-05-15 13:31:29 +0700

    first commit

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -9,6 +9,7 @@ route.post('/input', multer.single('img'), (req, res) => {
     let filepath = '../uploads/' + req.file.filename;
     let email = req.query.email;
     let content = req.body.content;
+
     const query = `INSERT INTO report (Email, Content, Image, Likes) VALUES ("${email}","${content}","${filepath}", 0);`;
     db.query(query, (err,result) => {
         if(err)throw err;
@@ -34,30 +35,52 @@ route.get('/:id', (req, res) => {
     })
 })
 
-// route.put('/:id', (req, res) => {
-//     const query = `UPDATE report set Likes = `;
-// })
+route.put('/:id/likes', (req, res) => {
+    const query = `UPDATE report SET Likes = Likes+${req.query.like} WHERE ReportID = ${req.params.id}`;
+    database.query(query, (err, result) => {
+        if(err)throw err;
+        res.status(200).send("Likes Inserted");
+    })
+})
 
-route.delete('/{id}', (req, res) => {
-    const query = `DELETE FROM report WHERE ReportID = ${req.params.reportId}`;
+route.delete('/:id', (req, res) => {
+    const query = `DELETE FROM report WHERE ReportID = ${req.params.id}`;
     database.query(query, (err, result) => {
         if(err) throw err;
-        res.status(200).send(`Data ID ${req.params.reportId} deleted.`);
+        res.status(200).send(`Data ID ${req.params.id} deleted.`);
     })
 })
 
 route.post('/:id/comment/input', (req, res) => {
-    let email = req.query.email;
-    let reportId = req.params.id;
-    // let content = req.body.content;
-
-    return res.json(req.query);
-
-    const query = `INSERT INTO comment (Email, ReportID, Content) VALUES ("${email}","${reportId}","${content}");`
+    const query = `INSERT INTO comment(ReportID, Content, Email) VALUES ("${req.params.id}", "${req.query.content}", "${req.query.email}")`;
     database.query(query, (err, result) => {
         if(err)throw err;
-        res.status(200).send('Comment inserted');
-    })
+        res.status(200).send("Comment Inserted");
+    });
+});
+
+route.delete('/:id/comment/:commentId', (req, res) => {
+    const query = `DELETE FROM comment WHERE CommentID = ${req.params.commentId}`;
+    database.query(query, (err, result) => {
+        if(err)throw err;
+        res.status(200).send("Comment Deleted");
+    });
+});
+
+route.get('/:id/comment/', (req, res) => {
+    const query = `SELECT * FROM comment WHERE ReportID = ${req.params.id}`;
+    database.query(query, (err, result) => {
+        if(err)throw err;
+        res.status(200).send(result);
+    });
+});
+
+route.get('/:id/comment/:commentId', (req, res) => {
+    const query = `SELECT * FROM comment WHERE ReportID = ${req.params.id} AND CommentID = ${req.params.commentId}`;
+    database.query(query, (err, result) => {
+        if(err)throw err;
+        res.status(200).send(result);
+    });
 });
 
 module.exports = route;
